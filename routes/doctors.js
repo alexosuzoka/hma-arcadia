5710e352ff21ea440bbf6c9372db2525a25c4ce0
alexosuzoka <alexosuzoka@gmail.com>
2025-07-27 03:51:17 +0000

    doctorchanges

--- routes/doctors.js
+++ routes/doctors.js
@@ -17,6 +17,48 @@ router.route('/doctors').get((req, res) => {
 router.route('/add')
      .post((req, res) => {
         const {name, specialty} = req.body;
+
+        const newDoctor = new Doctor({ name, specialty});
+
+        newDoctor.save()
+        // Return the savedDoctor object\
+           .then(savedDoctor =>{
+               res.json(savedDoctor)
+            .catch(
+                erro =>
+                    res.status(400)
+                          .json('Error:' + err)                     
+            );
+           });
+
+        // Update doctor data
+        router.route('/update/:id')
+            .post((req, res) => {
+                 Doctor.findById(req.params.id)
+                     .then(doctor => {
+                        if (!doctor) {
+                            return res.status(404)
+                                .json('Doctor not found');
+                        }
+
+                        doctor.name = req.body.name;
+                        doctor.specialty = req.body.speciality;
+
+                        doctor.save()
+                            .then(() => {
+                                res.json('Doctor updated!')
+                            })
+                            .catch(err => {
+                                res.status(400)
+                                   .json('Error: ' + err)
+                            })
+
+                        .catch(err => {
+                            res.status(400)
+                                .json('Error:' + err):
+                        })
+                     })
+            })
         
      })
 
